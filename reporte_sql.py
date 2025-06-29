845419d7f3c50359463a4e7d8c9ce9f4bdfd78b9
alesanhu <alesanhu@gmail.com>
2025-06-29 02:31:17 -0400

    Update reporte_sql.py

--- reporte_sql.py
+++ reporte_sql.py
@@ -1,6 +1,7 @@
 
 import streamlit as st
 import pandas as pd
+import sqlalchemy 
 from sqlalchemy import create_engine, text
 
 ###############################################################################
